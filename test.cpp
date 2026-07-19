c7dcb8ace21c3e1d75ab87b772024648a27ee6ea
yessin-rekik <yassine.rekik@ipsas.tn>
2026-07-19 21:56:42 +0100

    Message Handling
    custom WndProc for message handling, only VM_CLOSE in this commit

--- test.cpp
+++ test.cpp
@@ -1,6 +1,22 @@
 
 #include <Windows.h>
 
+
+//  window proc for handling messages
+LRESULT CALLBACK WndProc (HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+        // close button clicked
+        case WM_CLOSE:        
+            PostQuitMessage( 69 );
+            break;
+    }
+
+    // pass back to default window proc for default handling
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
     HINSTANCE hInstance, 
     HINSTANCE hPrevInstance,
@@ -15,7 +31,7 @@ int CALLBACK WinMain(
     wc.cbSize = sizeof(wc);
 
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -37,6 +53,22 @@ int CALLBACK WinMain(
 
     ShowWindow (hWnd, SW_SHOW);
 
-    while (true);
-    return 0;
+    // messages
+
+    MSG msg;
+
+    // check if result promps for ending program
+    BOOL gResult;
+    while ( (gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    if (gResult == -1)
+    {
+        return -1;
+    } else {
+        return msg.wParam;
+    }
 }
