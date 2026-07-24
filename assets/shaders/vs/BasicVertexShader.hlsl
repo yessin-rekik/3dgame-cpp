435bd01a60dbaa25446bb2e3b9acfb68158ab6fc
yessin-rekik <yassine.rekik@ipsas.tn>
2026-07-24 23:30:14 +0100

    Shaders
    GPU language or whatever
    
    Two small GPU programs, not yet wired into any C++ code or the build.

---
+++ assets/shaders/vs/BasicVertexShader.hlsl
@@ -0,0 +1,16 @@
+struct VS_INPUT
+{
+    float3 position : POSITION;
+};
+
+struct VS_OUTPUT
+{
+    float4 position : SV_POSITION;
+};
+
+VS_OUTPUT main(VS_INPUT input)
+{
+    VS_OUTPUT output;
+    output.position = float4(input.position, 1.0f);
+    return output;
+}
